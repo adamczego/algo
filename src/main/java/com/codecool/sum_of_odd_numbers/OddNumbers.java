7eb32aa350b3fb4867169ef85badc8d126c70be6
adamczego <cz4d4m@gmail.com>
2021-03-24 15:07:51 +0100

    complete OddNumbers

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+        return numbers.stream()
+            .filter(num -> num % 2 != 0)
+            .reduce(Integer::sum)
+            .orElse(0);
     }
+
 }
